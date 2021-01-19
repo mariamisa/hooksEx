4e50054ed5ef7d85c34f933e9ffc68f34b3f3957
mariamisa <maryaam1998@hotmail.com>
2021-01-20 01:24:15 +0200

    add edit user function

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -6,6 +6,13 @@ import InputFeild from "../InputFeild";
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [id, setId] = useState();
+
+  const [show, setShow] = useState(false);
+
   useEffect(() => {
     let isCurrent = true;
     fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
@@ -24,26 +31,92 @@ export default function Users() {
     };
   }, []);
 
+  const fillALlStateAndShowForm = (id, name, email, phone) => {
+    setId(id);
+    setName(name);
+    setEmail(email);
+    setPhone(phone);
+    setShow(true);
+  };
+
   const handelDelete = (id) => setUsers(users.filter((el) => el.id !== id));
-  const editUser = (id) => {};
+
+  const handelChangeState = (value, stateCb) => stateCb(value);
+
+  const handelSaveChanges = () => {
+    const editedUser = users.map((user) => {
+      if (user.id === id) {
+        user.name = name;
+        user.email = email;
+        user.phone = phone;
+        return user;
+      }
+      return user;
+    });
+    setUsers(editedUser);
+    setShow(false);
+  };
 
   return (
-    <div>
-      {error && <p>{error}</p>}
-      {users &&
-        users.map((user) => (
-          <div>
-            <p>user name : {user.name}</p>
-            <p>email : {user.email}</p>
-            <p>phone : {user.phone}</p>
-            <Button btnName="edit" handelClick={() => editUser(user.id)}>
-              edit
-            </Button>
-            <Button btnName="delete" handelClick={() => handelDelete(user.id)}>
-              delete
-            </Button>
-          </div>
-        ))}
-    </div>
+    <>
+      {!show ? (
+        <div>
+          {error && <p>{error}</p>}
+          {users &&
+            users.map(({ id, name, email, phone }) => (
+              <div>
+                <p>user name : {name}</p>
+                <p>email : {email}</p>
+                <p>phone : {phone}</p>
+                <Button
+                  btnName="edit"
+                  handelClick={() =>
+                    fillALlStateAndShowForm(id, name, email, phone)
+                  }
+                >
+                  edit
+                </Button>
+                <Button btnName="delete" handelClick={() => handelDelete(id)}>
+                  delete
+                </Button>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <form>
+          <InputFeild
+            value={name}
+            inputName="name"
+            placeholder="enter your name"
+            type="text"
+            handelChange={({ target: { value } }) =>
+              handelChangeState(value, setName)
+            }
+          />
+          <br />
+          <InputFeild
+            value={email}
+            placeholder="enter your email"
+            inputName="eamil"
+            type="eamil"
+            handelChange={({ target: { value } }) =>
+              handelChangeState(value, setEmail)
+            }
+          />
+          <br />
+          <InputFeild
+            value={phone}
+            inputName="phone"
+            type="text"
+            placeholder="enter your phone"
+            handelChange={({ target: { value } }) =>
+              handelChangeState(value, setPhone)
+            }
+          />
+          <br />
+          <Button handelClick={handelSaveChanges}>save changes</Button>
+        </form>
+      )}
+    </>
   );
 }
